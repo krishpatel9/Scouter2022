5d86645cb6d7ee386b00d7ae83f00e03b8c9551d
krish <patelkrish@htps.us>
2022-03-24 12:58:48 -0400

    MOBILE PHONE COMPATIBILITY v2

--- app/src/main/java/com/example/scouter2022/util/Defaults.java
+++ app/src/main/java/com/example/scouter2022/util/Defaults.java
@@ -2,7 +2,7 @@ package com.example.scouter2022.util;
 
 public class Defaults {
     public static final int MAX_TEAM_NUMBER = 16383;
-    public static final int MAX_MATCH_NUMBER = 127;
+    public static final int MAX_MATCH_NUMBER = 200;
     public static final int MAX_CARGO_NUMBER = 100;
     public static final int MAX_FOULS = 100;
     public static final String BLUE_ALLIANCE = "BLUE ALLIANCE";
